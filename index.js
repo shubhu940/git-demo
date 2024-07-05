3330fcafa09a2f993eb752a9bf119823cbbeeeeb
Shubham Bhange <shubhambhange887@gmail.com>
2024-07-05 17:22:09 +0530

    Revert SOmething

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const age = 24;
 
+
+
 function sayHello() {
     const name1 = 'shubhu';
     console.log('My name is' + name)
 }
 
+
+
+function isAbove18() {
+    return age >= 18;
+}
 function newFn() {
     console.log("Hey I am a new fn");
 }
